8792a27404c8ddd3da217dfbb04ac479c710fb9b
Furqan Baqai <baqai.furqan@gmail.com>
2019-01-26 14:16:50 +0400

    Socket.io lecture number 107

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;
 
@@ -7,14 +10,27 @@ const port = process.env.PORT || 3000;
 // Setup Express 
 var app = express(); // Declared on line #2
 
+// We will decouple HTTP listener from the app so that 
+// we can hook up the socket.io listener
+var server = http.createServer(app);
+// Configure server to use socket.io
+var io = socketIO(server);
+
 // Configuring Static middleware
 // Ref: http://expressjs.com/en/starter/static-files.html
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) =>{
+  console.log('New user connected');
+
+  socket.on('disconnect', () =>{
+    console.log('Client disconnected');
+  });
+});
 
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Started express on port ${port}`);
 });
 
